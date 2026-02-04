e78f49b064b85d76f1b20047837975ecc916f199
krittika <pandiankrittika@gmail.com>
2026-02-04 08:04:52 +0530

    Fix syntax error in multiplication output

--- Test.java
+++ Test.java
@@ -15,7 +15,7 @@ public class Test {
     // Multiplication
     static void mul(int a, int b) {
         int res = a * b;
-        System.out.println("Multiplication result: " + res);
+        System.out.println("Multiplication result: " + res)
     }
 
     // Division
